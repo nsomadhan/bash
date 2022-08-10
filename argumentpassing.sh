1906adeb4bec5ac46feeda62d2437b3eb141adf5
nsomadhan <nsomadhan@gmail.com>
2022-08-10 18:22:03 +0600

    argument passing

--- argumentpassing.sh
+++ argumentpassing.sh
@@ -4,3 +4,14 @@ echo 'First argument is : '  $1
 echo 'Second argument is : ' $2
 echo 'Third argument is : '  $3
 echo 'Fourth argument is : ' $4
+
+# argument store in variable 
+args=("$@")
+echo 'argument_'${args[0]} 
+array=(arrayvalueone arrayvaluetwo) # array declaration
+echo 'array__'${array[0]} 'argument__'${args[1]}
+
+
+
+# Calculate number off argument passed
+echo 'Number of argument pass': $#
